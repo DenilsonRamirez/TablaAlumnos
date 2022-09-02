edc4c283f3e1cff350b8b87c5733007be892b36d
P! <denixmas123@gmail.com>
2022-09-01 20:11:29 -0600

    Delete clase25Agostro-main directory

--- clase25Agostro-main/EstudianteController.php
+++
@@ -1,4 +0,0 @@
-<?php
-include "EstudianteModel.php";
-$nuevoEstudiante = new Estudiante();
-$nuevoEstudiante->GuardarEstudiante($_POST['apellidos'],$_POST['nombre'],$_POST['direccion'],$_POST['telefono']);
